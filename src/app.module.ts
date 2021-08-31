7741d818b2e42c02776ccfb98473edafcbe02f67
Stanislav <verbatim8872@gmail.com>
2021-08-31 17:18:47 +0300

    add schemas and connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { TrackModule } from './track/track.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [TrackModule]
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.19tcc.mongodb.net/Music-player?retryWrites=true&w=majority'),
+        TrackModule
+    ]
 })
 export class AppModule{
     
