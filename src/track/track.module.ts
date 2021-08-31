ed22313e24fdd2c368dd3a25d3f9394e3eba4e65
Stanislav <verbatim8872@gmail.com>
2021-08-31 17:22:41 +0300

    connect schemas to trackModule

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,9 +1,16 @@
+import { Comments, CommentsSchema } from './schemas/comments.schema';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Track, TrackSchema } from './schemas/track.schema';
 
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Track.name, schema: TrackSchema}]),
+        MongooseModule.forFeature([{ name: Comments.name, schema: CommentsSchema}])
+    ],
     controllers: [TrackController],
     providers: [TrackService]
 })
